d5d2242f48c1b7a7748410dd231d49a1364c734b
aweeks2018@gmail.com <aweeks2018@gmail.com>
2026-04-03 22:28:17 -0400

    Now we don't go to the top of the page every time we refresh

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+import os
+
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=('cert.pem', 'key.pem'))
+    cert_file = 'cert.pem'
+    key_file = 'key.pem'
+    ssl_context = (cert_file, key_file) if os.path.exists(cert_file) and os.path.exists(key_file) else None
+
+    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=ssl_context)
